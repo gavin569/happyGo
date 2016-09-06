0a14053bc31d8a90c3816cdf326291ccf4bb7388
edianzu <gavin_only@163.com>
2016-09-06 10:58:28 +0800

    Initial Commit

---
+++ TravelAgency/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelAgency
+//
+//  Created by edianzu on 16/9/6.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
